5191ed613a09a12e2361c3fcdd7b4451c854197b
Carolina <carolinacarreira@Carolinas-MacBook-Air.local>
2018-03-14 11:16:51 +0000

    Adicionei codigo do stack

---
+++ stack.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct stacknode {
+	int content;
+	struct stacknode *next;
+
+}*node;
+
+
+
+node init (int c) {
+	node head = malloc (sizeof(struct stacknode));
+	head->content = c;
+	head->next = NULL;
+	return head;
+}
+
+
+
+
+void push (node *head, int c) {
+	node new = malloc (sizeof(struct stacknode));
+	new->next = *head;
+	new->content = c;
+	*head = new;
+}
+
+int pop (node *head) {
+	if (*head == NULL) {
+		fprintf(stderr, "Pop num stack vazio\n");
+		exit (-1);
+	}
+	node aux = *head;
+	int value = aux->content;
+	*head = (*head)->next;
+	free(aux);
+	return value;
+}
+
+void printStack(node head) {
+	node aux = head;
+	while (aux != NULL) {
+		printf("%d\n", aux->content);
+		aux = aux->next;
+	}
+}
+
+int main () {
+	node head = init (3);
+	push (&head, 4);
+	push (&head, 5);
+	printStack(head);
+	//print(head);
+	printf("POP %d\n",pop(&head));
+	printStack(head);
+	//printf("%d",pop(head));
+	pop (&head);
+	pop (&head);
+
+
+
+
+
+	return 0;
+}
+
